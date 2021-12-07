306e66c3bfdeb92891ec2fc2ae007a76fc7caafd
Tommy <rejectmusician@hotmail.co.uk>
2021-12-07 10:32:18 +0000

    Day 7 finished - easy peasy! :)

---
+++ day07/sol.py
@@ -0,0 +1,29 @@
+import sys
+
+crabs = []
+part = 2
+
+for line in sys.stdin:
+    for n in line.strip().split(','):
+        crabs.append(int(n))
+
+min_possible = min(crabs) + 1
+max_possible = max(crabs) - 1
+
+best = -1
+
+def triangular_number(n):
+    return (n * (n + 1) / 2)
+
+
+for n in range(min_possible, max_possible):
+    tot = 0
+
+    for c in crabs:
+        if (part == 1): tot += abs(c - n)
+        else: tot += triangular_number(abs(c - n))
+
+    if best == -1 or tot < best:
+        best = tot
+
+print('part', part, ':', best)
